a04a376bcf6ec9f5c76bc1815e09f7a1ef59c520
Idel Montalvo <idel_m@hotmail.com>
2023-09-06 12:07:35 +0200

    starting idea for custom map tools

---
+++ maptools/insertNodeAbstractTool.py
@@ -0,0 +1,30 @@
+
+""" class PointMapTool(QgsMapToolEmitPoint):
+
+    def __init__(self, canvas):
+
+        self.canvas = canvas
+
+        QgsMapToolEmitPoint.__init__(self, self.canvas)
+
+        self.point = None
+
+
+
+    def canvasPressEvent(self, e):
+
+        self.point = self.toMapCoordinates(e.pos())
+
+        print(self.point.x(), self.point.y())
+
+        m = QgsVertexMarker(self.canvas)
+
+        m.setCenter(self.point)
+
+        m.setColor(QColor(0,255,0))
+
+        m.setIconSize(5)
+
+        m.setIconType(QgsVertexMarker.ICON_BOX) # or ICON_CROSS, ICON_X
+
+        m.setPenWidth(3) """
