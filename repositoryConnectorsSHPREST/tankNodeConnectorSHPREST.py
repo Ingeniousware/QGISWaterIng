b0f5a9bf5c8ea44f1a5bd2bd60b915bee61adbfd
vitorv7519 <vitorestradas@hotmail.com>
2023-12-07 02:49:24 -0300

    tanks - lastUpdate first set after posting

--- repositoryConnectorsSHPREST/tankNodeConnectorSHPREST.py
+++ repositoryConnectorsSHPREST/tankNodeConnectorSHPREST.py
@@ -105,16 +105,17 @@ class tankNodeConnectorSHPREST(abstractRepositoryConnectorSHPREST):
                 id_element = feature["ID"]
                 
                 layer.startEditing()
-
+                
                 c_feature = None
                 for feat in layer.getFeatures():
                     if feat["ID"] == id_element:
                         c_feature = feat
                         c_feature.setAttribute(c_feature.fieldNameIndex("ID"), str(serverKeyId))
+                        c_feature.setAttribute("lastUpdate", WateringUtils.getDateTimeNow())
                         layer.updateFeature(c_feature)
                         print("Feature Found")
                         break
-                
+                    
                 layer.commitChanges()
         
             if not serverKeyId in self.lastAddedElements:     
