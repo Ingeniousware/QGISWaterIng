4f4745f231845258b0295486863eafc1b859ebb9
vitorv7519 <vitorestradas@hotmail.com>
2024-01-11 00:39:21 -0300

    differentiation between online and offline adding and deletion

--- repositoriesLocalSHP/syncSystem.py
+++ repositoriesLocalSHP/syncSystem.py
@@ -1,5 +1,7 @@
+from qgis.core import QgsProject
 from ..watering_utils import WateringUtils
 from collections import deque
+import json
 
 class Change:
     def __init__(self, layer_id, feature_id, change_type, data):
@@ -13,10 +15,12 @@ class WateringSync:
     def __init__(self):
         self.change_queue = deque()
         self.change_handlers = {
-            "add": self.process_add,
+            "add_server": self.process_add_to_server,
+            "add_offline": self.process_add_to_offline,
             "update_server": self.process_update_on_server,
             "update_offline": self.process_update_in_offline,
-            "delete": self.process_delete
+            "delete_server": self.process_delete_on_server,
+            "delete_offline": self.process_delete_in_offline
         }
         
     def trackChange(self, feature_id, change_type):
@@ -35,15 +39,20 @@ class WateringSync:
         except KeyError:
             print(f"Unknown change type: {change.change_type}")
 
-    def process_add(self, change):
+    def process_add_to_server(self, change):
         ...
 
+    def process_add_to_offline(self, change):
+        ...
+        
     def process_update_on_server(self, change):
         ...
 
     def process_update_in_offline(self, change):
         ...
         
-    def process_delete(self, change):
+    def process_delete_on_server(self, change):
         ...
         
+    def process_delete_in_offline(self, change):
+        ...
