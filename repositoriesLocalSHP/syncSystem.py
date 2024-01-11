9427056938a904ad5d54a879bb279712cd0566d5
vitorv7519 <vitorestradas@hotmail.com>
2024-01-10 23:33:53 -0300

    WateringSync improvements

--- repositoriesLocalSHP/syncSystem.py
+++ repositoriesLocalSHP/syncSystem.py
@@ -2,7 +2,8 @@ from ..watering_utils import WateringUtils
 from collections import deque
 
 class Change:
-    def __init__(self, feature_id, change_type, timestamp=None):
+    def __init__(self, layer_id, feature_id, change_type, timestamp=None):
+        self.layer_id = layer_id
         self.feature_id = feature_id
         self.change_type = change_type
         self.timestamp = WateringUtils.getDateTimeNow()
@@ -10,7 +11,12 @@ class Change:
 class WateringSync:
     def __init__(self):
         self.change_queue = deque()
-
+        self.change_handlers = {
+            "add": self.process_add,
+            "update": self.process_update,
+            "delete": self.process_delete
+        }
+        
     def trackChange(self, feature_id, change_type):
         change = Change(feature_id, change_type)
         self.change_queue.append(change)
@@ -21,13 +27,10 @@ class WateringSync:
             self.processChange(change)
 
     def processChange(self, change):
-        if change.change_type == "add":
-            self.process_add(change)
-        elif change.change_type == "update":
-            self.process_update(change)
-        elif change.change_type == "delete":
-            self.process_delete(change)
-        else:
+        try:
+            process_method = self.change_handlers[change.change_type]
+            process_method(change)
+        except KeyError:
             print(f"Unknown change type: {change.change_type}")
 
     def process_add(self, change):
