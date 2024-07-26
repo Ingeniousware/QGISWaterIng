be8d935e8a9db8879fb786231380e0cc5d4f1505
vitorv7519 <vitorestradas@hotmail.com>
2024-07-26 02:12:28 -0300

    first tab merging nodeXnode working

--- ui/watering_selection_review.py
+++ ui/watering_selection_review.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
-
 from qgis.PyQt import uic, QtWidgets
-from qgis.core import QgsProject, Qgis, QgsPointXY, QgsVectorLayer, QgsField, QgsFeature, QgsGeometry
+from qgis.core import QgsProject, Qgis, QgsField, QgsFeatureRequest
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import Qt, QSortFilterProxyModel
 from qgis.utils import iface
-from qgis.gui import QgsVertexMarker
-from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QHeaderView
 
 import os
+from ..watering_utils import WateringUtils
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'watering_selection_review_dialog.ui'))
@@ -28,6 +25,7 @@ class WateringSelectionReview(QtWidgets.QDialog, FORM_CLASS):
         self.populateTableView(self.node_pipe_list, ["Node", "Pipe", "Distance", "Node_fID", "Pipe_fID"], self.tableViewNodeVsPipe)
         self.nodeVsNodeCheckBox.stateChanged.connect(self.updateCheckboxText)
         self.nodeVsPipeCheckBox.stateChanged.connect(self.updateCheckboxText)
+        self.NodeXNodeBtn.clicked.connect(self.onNodeXNodeBtnClicked)
 
     def populateTableView(self, data_list, headers, table_view):
         features_count = len(data_list)
@@ -44,7 +42,41 @@ class WateringSelectionReview(QtWidgets.QDialog, FORM_CLASS):
 
             table_view.setColumnHidden(len(headers) - 2, True)  # Hide Node1_fID
             table_view.setColumnHidden(len(headers) - 1, True)  # Hide Node2_fID
+   
+    def onNodeXNodeBtnClicked(self):
+        selected_row = self.tableViewNodeVsNode.selectionModel().currentIndex().row()
+        if selected_row != -1:
+            node1_fID = int(self.tableViewNodeVsNode.model().data(self.tableViewNodeVsNode.model().index(selected_row, 3)))
+            node2_fID = int(self.tableViewNodeVsNode.model().data(self.tableViewNodeVsNode.model().index(selected_row, 4)))
+            if self.nodeVsNodeCheckBox.isChecked():
+                self.mergeNodes(node1_fID, node2_fID, True)
+            else:
+                self.mergeNodes(node1_fID, node2_fID, False)
+        else:
+            WateringUtils.error_message("No selection. Please select a row in the table.")
+
+    def mergeNodes(self, node1_fID, node2_fID, merge_node2_into_node1):
+        layer = QgsProject.instance().mapLayersByName('watering_demand_nodes')[0]
+        node1 = next(layer.getFeatures(QgsFeatureRequest(node1_fID)))
+        node2 = next(layer.getFeatures(QgsFeatureRequest(node2_fID)))
+        
+        if node1 and node2:
+            if merge_node2_into_node1:
+                target_node, source_node = node1, node2
+            else:
+                target_node, source_node = node2, node1
             
+            #target_node.setGeometry(target_node.geometry().combine(source_node.geometry()))
+            
+            layer.startEditing()
+            layer.updateFeature(target_node)
+            layer.deleteFeature(source_node.id()) #TODO - create Watering Utils delete feature
+            layer.commitChanges()
+            
+            WateringUtils.success_message("Nodes merged successfully")
+        else:
+            WateringUtils.error_message("One or both nodes not found")
+
     def on_row_clicked(self, index, table_view):
         if index.isValid():
             row = index.row()
