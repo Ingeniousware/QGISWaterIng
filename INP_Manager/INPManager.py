467626ea14bcda0f2d956632a25ced1e3fe6cfac
carlosperezlantigua <cperezlantigua@gmail.com>
2024-12-22 21:48:36 -0500

    update plugn_inpfile nuve 22122024-2148

--- INP_Manager/INPManager.py
+++ INP_Manager/INPManager.py
@@ -16,27 +16,35 @@
 ***************************************************************************
 """
 
-from qgis.core import QgsProject
+from qgis.core import QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsFields, QgsRenderContext, QgsVectorLayer, QgsProject
 
 from .sections import sectionTitle, sectionJunctions, sectionReservoirs, sectionTanks, sectionPipes, sectionPumps, sectionValves, sectionTags, sectionDemands, sectionStatus, sectionPatterns, sectionCurves, sectionControls, sectionRules, sectionEnergy, sectionEmitters, sectionQuality, sectionSources, sectionReactions, sectionReactions20, sectionMixing, sectionTimes, sectionReport, sectionOptions, sectionCoordinates, sectionVertices, sectionLabels, sectionBackdrop, sectionEnd
-from .dataType import Junction, Reservoir, Tank, Pipe, Pump, Valve, Demand, Curve, Coordinate, Vertice, Label
+from .dataType import Junction, Reservoir, Tank, Pipe, Pump, Valve, Tag, Demand, Curve, Coordinate, Vertice, Label, Backdrop
 
 class INPManager():
     def __init__(self, outfile=None):
         self.outfile = outfile
+        
+        #list1.First(ii => ii.id == "")
+        #next((item for item in list1 if item.id == ""), None)
+        
+        self.xmin = 0.0
+        self.ymin = 0.0
+        self.xmax = 10000.0
+        self.ymax = 10000.0
 
-        self.sections = {'TITLE': sectionTitle('PRUEBA DE INP'),# - 1 - Ok
-                         'JUNCTIONS': sectionJunctions(),# - 1 - Ok
-                         'RESERVOIRS': sectionReservoirs(),# - 1- Ok
-                         'TANKS': sectionTanks(),# - 1 - Ok
-                         'PIPES': sectionPipes(),# - 1 - Ok
-                         'PUMPS': sectionPumps(),# - 1 - Ok
-                         'VALVES': sectionValves(),# - 1 - Ok
+        self.sections = {'TITLE': sectionTitle('PRUEBA DE INP'),
+                         'JUNCTIONS': sectionJunctions(),
+                         'RESERVOIRS': sectionReservoirs(),
+                         'TANKS': sectionTanks(),
+                         'PIPES': sectionPipes(),
+                         'PUMPS': sectionPumps(),
+                         'VALVES': sectionValves(),
                          'TAGS': sectionTags(),
-                         'DEMANDS': sectionDemands(),# - 1 - Ok
+                         'DEMANDS': sectionDemands(),
                          'STATUS': sectionStatus(),
                          'PATTERNS': sectionPatterns(),
-                         'CURVES': sectionCurves(),# - 1 - Ok
+                         'CURVES': sectionCurves(),
                          'CONTROLS': sectionControls(),
                          'RULES': sectionRules(),
                          'ENERGY': sectionEnergy(),
@@ -48,66 +56,213 @@ class INPManager():
                          'MIXING': sectionMixing(),
                          'TIMES': sectionTimes(),
                          'REPORT': sectionReport(),
-                         'OPTIONS': sectionOptions(), # - 1 - Ok
-                         'COORDINATES': sectionCoordinates(),# - 1 - Ok
-                         'VERTICES': sectionVertices(),# - 1 - Ok
-                         'LABELS': sectionLabels(),# - 1 - Ok
+                         'OPTIONS': sectionOptions(),
+                         'COORDINATES': sectionCoordinates(),
+                         'VERTICES': sectionVertices(),
+                         'LABELS': sectionLabels(),
                          'BACKDROP': sectionBackdrop(),
                          'END': sectionEnd()}
-        #Testing para las sections.
+        
+    def __readFeatures(self, layerName):
+        source_layer = QgsProject.instance().mapLayersByName(layerName)[1]
+        print(source_layer)
+        return source_layer.getFeatures()
+    
+    def __readDemandNode(self, layerName = "watering_demand_nodes"):
+        source_layer = QgsProject.instance().mapLayersByName(layerName)[0]
+        features = source_layer.getFeatures() #self.__readFeatures(layerName)
+        # Define the source and destination coordinate reference systems
+        crs_source = source_layer.crs()
+        crs_destination = QgsCoordinateReferenceSystem("EPSG:4326")  # WGS84
+        transform = QgsCoordinateTransform(crs_source, crs_destination, QgsProject.instance())
+            
+        coordinate = self.sections['COORDINATES']
         junctions = self.sections['JUNCTIONS']
-        for i in range(1, 5):
-            junctions.values.append(Junction(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1)))
+        tag = self.sections['TAGS']
         
-        reservoirs = self.sections['RESERVOIRS']
-        for i in range(1, 5):
-            reservoirs.values.append(Reservoir(i, (i + 1 * 2), (i * 3 + 1)))
+        for feature in features:
+            pointXY = feature.geometry().asPoint()
+            #transformed_point = transform.transform(pointXY)
+            #print(f"Imprimiento puntos {transformed_point}")
         
-        tank = self.sections['TANKS']
-        for i in range(1, 5):
-            tank.values.append(Tank(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1), (i * 2 + 3), (i * 4 + 4), (i * 5 + 1), (i * 2 + 5)))
+            id = feature.attribute("Name") #if feature.attribute("Name") is not None else ""
+            elev = feature.attribute("Z[m]") #if feature.attribute("Z[m]") is not None else 0.0
+            demand = feature.attribute("B. Demand") #if feature.attribute("B. Demand") is not None else ""
+            pattern = feature.attribute("EmitterCoe") #if feature.attribute("EmitterCoe") is not None else ""
+            description = feature.attribute("Descript") #if feature.attribute("Descript") is not None else ""
+            label = "" #Esto es para escribir las etiquetas de epanet.
+            
+            coordinate.values.append(Coordinate(id, pointXY.x(), pointXY.y()))
+            
+            junctions.values.append(Junction(id, elev, demand, pattern, description))
+            
+            if label != "":
+                tag.values.append(Tag("NODE", id, label))
+    
+    def __readReservoirs(self, layerName = "watering_reservoirs"):
         
-        pipe = self.sections['PIPES']
-        for i in range(1, 5):
-            pipe.values.append(Pipe(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1), (i * 2 + 3), (i * 4 + 4), (i * 5 + 1), (i * 2 + 5)))
-        
-        pump = self.sections['PUMPS']
-        for i in range(1, 5):
-            pump.values.append(Pump(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1)))
-        
-        valve = self.sections['VALVES']
-        for i in range(1, 5):
-            valve.values.append(Valve(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1), (i * 2 + 3), (i * 4 + 4), (i * 5 + 1)))
-        
-        demand = self.sections['DEMANDS']
-        for i in range(1, 5):
-            demand.values.append(Demand(i, (i + 1 * 2), (i * 2 + 1), (i * 3 + 1)))
-        
-        curve = self.sections['CURVES']
-        for i in range(1, 5):
-            curve.values.append(Curve(i, (i + 1 * 2), (i * 2 + 1)))
+        source_layer = QgsProject.instance().mapLayersByName(layerName)[1]
+        features = source_layer.getFeatures() #self.__readFeatures(layerName)
         
         coordinate = self.sections['COORDINATES']
-        for i in range(1, 5):
-            coordinate.values.append(Coordinate(i, (i + 1 * 2), (i * 2 + 1)))
+        reservoirs = self.sections['RESERVOIRS']
+        tag = self.sections['TAGS']
         
+        for feature in features:
+            pointXY = feature.geometry().asPoint()
+            id = feature.attribute("Name") #if feature.attribute("Name") is not None else ""
+            #elev = feature.attribute("Z[m]") if feature.attribute("Z[m]") is not None else ""
+            head = feature.attribute("Head[m]") #if feature.attribute("Head[m]") is not None else 0.0
+            pattern = ""
+            description = feature.attribute("Descript") #if feature.attribute("Descript") is not None else ""
+            label = "" #Esto es para escribir las etiquetas de epanet.
+            
+            # layer = QgsProject.instance().mapLayersByName("watering_reservoirs")[1]
+            # layer_tree_root = QgsProject.instance().layerTreeRoot()
+            # layer_tree_layer = layer_tree_root.findLayer(layer)
+            # #layer_is_visible = layer_tree_layer.isVisible()
+            # #print(layer_is_visible)
+            # visibleFeatures = []
+            # renderer = layer.renderer().clone()
+            # ctx = QgsRenderContext()
+            # renderer.startRender(ctx, QgsFields())
+            # for feature in layer.selectedFeatures():
+            #     ctx.expressionContext().setFeature(feature)
+            #     if renderer.willRenderFeature(feature, ctx):
+            #         visibleFeatures.append(feature)
+            # renderer.stopRender(ctx)
+            # print(visibleFeatures)
+            # visibleFeatures.append("Carlos")
+            # print(visibleFeatures)
+            
+            coordinate.values.append(Coordinate(id, pointXY.x(), pointXY.y()))
+            
+            reservoirs.values.append(Reservoir(id, head, pattern, description))
+            
+            if label != "":
+                tag.values.append(Tag("NODE", id, label))
+
+    def __readTanks(self, layerName = "watering_tanks"):
+        
+        source_layer = QgsProject.instance().mapLayersByName(layerName)[0]
+        features = source_layer.getFeatures() #self.__readFeatures(layerName)
+        
+        coordinate = self.sections['COORDINATES']
+        tank = self.sections['TANKS']
+        tag = self.sections['TAGS']
+        
+        for feature in features:
+            pointXY = feature.geometry().asPoint()
+            id = feature.attribute("Name") #if feature.attribute("Name") is not None else ""
+            elevation = feature.attribute("Z[m]") #if feature.attribute("Z[m]") is not None else 0.0
+            initLevel = feature.attribute("Init. Lvl") #if feature.attribute("Init. Lvl") is not None else 0.0
+            minLevel = feature.attribute("Min. Lvl") #if feature.attribute("Min. Lvl") is not None else 0.0
+            maxLevel = feature.attribute("Max. Lvl") #if feature.attribute("Max. Lvl") is not None else 0.0
+            diameter = feature.attribute("Diameter") #if feature.attribute("Diameter") is not None else 0.0
+            minVol = feature.attribute("Min. Vol.") #if feature.attribute("Min. Vol.") is not None else 0.0
+            volCurve = ""
+            description = feature.attribute("Descript") #if feature.attribute("Descript") is not None else ""
+            label = "" #Esto es para escribir las etiquetas de epanet.
+            
+            coordinate.values.append(Coordinate(id, pointXY.x(), pointXY.y()))
+            
+            tank.values.append(Tank(id, elevation, initLevel, minLevel, maxLevel, diameter, minVol, volCurve, description))
+            
+            if label != "":
+                tag.values.append(Tag("NODE", id, label))
+    
+    def __readPipes(self, layerName = "watering_pipes"):
+        
+        source_layer = QgsProject.instance().mapLayersByName(layerName)[1]
+        features = source_layer.getFeatures() #self.__readFeatures(layerName)
+        
+        coordinate = self.sections['COORDINATES']
         vertice = self.sections['VERTICES']
-        for i in range(1, 5):
-            vertice.values.append(Vertice(i, (i + 1 * 2), (i * 2 + 1)))
+        pipe = self.sections['PIPES']
+        tag = self.sections['TAGS']
         
-        label = self.sections['LABELS']
-        for i in range(1, 5):
-            label.values.append(Label(i, (i + 1 * 2), (i * 2 + 1)))
+        for feature in features:
+            id = feature.attribute("Name") #if feature.attribute("Name") is not None else 0.0
+            node1 = feature.attribute("Up-Node") #if feature.attribute("Up-Node") is not None else 0.0
+            node2 = feature.attribute("Down-Node") #if feature.attribute("Down-Node") is not None else 0.0
+            length = feature.attribute("Length") #if feature.attribute("Length") is not None else 0.0
+            diameter = feature.attribute("Diameter") #if feature.attribute("Diameter") is not None else 0.0
+            roughness = feature.attribute("Rough.A") #if feature.attribute("Rough.A") is not None else 0.0
+            minorLoss = 0
+            status = "Open"
+            description = feature.attribute("Descript") #if feature.attribute("Descript") is not None else ""
+            label = "" #Esto es para escribir las etiquetas de epanet.
+            
+            geom = feature.geometry()
+            vert = 0
+            for part in geom.parts():
+                for vertex in part.vertices():
+                    if part.startPoint() == geom.vertexAt(vert):
+                        coordinate.values.append(Coordinate(node1, part.startPoint().x(), part.startPoint().y()))
+                    elif part.endPoint() == geom.vertexAt(vert):
+                        coordinate.values.append(Coordinate(node2, part.endPoint().x(), part.endPoint().y()))
+                    else:
+                        vertice.values.append(Vertice(id, vertex.x(), vertex.y()))
+                    vert += 1
+            
+            pipe.values.append(Pipe(id, node1, node2, length, diameter, roughness, minorLoss, status, description))
+            
+            """
+            pipe_feature.setAttribute("C(H.W.)", 0)
+            """
+            
+            if label != "":
+                tag.values.append(Tag("LINK", id, label))
+    
+    def __Pumps(self, layerName = "watering_pumps"):
+        features = self.__readFeatures(layerName)
+        for feature in features:
+            id = feature.attribute("Name") #if feature.attribute("Name") is not None else 0.0
+            node1 = feature.attribute("Up-Node") #if feature.attribute("Up-Node") is not None else 0.0
+            node2 = feature.attribute("Down-Node") #if feature.attribute("Down-Node") is not None else 0.0
+            #parameters
+        
+        
+        
+        """
+        ID              	Node1           	Node2           	Parameters
+        new_feature.setAttribute("Name", name)
+        new_feature.setAttribute("Descript", description)
+        new_feature.setAttribute("Z[m]", z)
+        new_feature.setAttribute("Model FK", modelFK)
+        new_feature.setAttribute("Rel.Speed", relativeSpeed)
+        """
+    
+    
+    def __readBackdrop(self):
+        coordinate = self.sections['COORDINATES']
+        self.xmin = self.xmax = coordinate.values[0].X_Coord
+        self.ymin = self.ymax = coordinate.values[0].Y_Coord
+        
+        self.xmin = min(item.X_Coord for item in coordinate.values)
+        self.ymin  = min(item.Y_Coord for item in coordinate.values)
+        
+        self.xmax = max(item.X_Coord for item in coordinate.values)
+        self.ymax = max(item.Y_Coord for item in coordinate.values)
+        
+        backdrop = self.sections['BACKDROP']
+        backdrop.values.append(Backdrop(self.xmin, self.ymin, self.xmax, self.ymax))
+    
+    def __readLayers(self):
+        self.__readDemandNode()
+        self.__readReservoirs()
+        self.__readTanks()
+        self.__readPipes()
+        
+        self.__readBackdrop()
 
     def writeSections(self, outfile=None):
         if outfile is not None:
             self.outfile = outfile
 
-        #Se debe organizar los sections antes de mandarlas a escribir, comprobar que todas las secciones esten escritas de no estar escrita se crea vacia.
-
+        self.__readLayers()
+        
         for t, s in self.sections.items():
             print(t,s.name)
             s.writeSection(self.outfile)
-
-    def readLayers(self):
-        source_layer = QgsProject.instance().mapLayersByName("watering_pipes")[0]
+            
--- INP_Manager/dataType.py
+++ INP_Manager/dataType.py
@@ -18,131 +18,181 @@
 
 class Junction():
     #ID              	Elev        	Demand      	Pattern
-    def __init__(self, id = 0, elev = 0, demand = 0, pattern = ""):
-        self.ID = id
-        self.Elev = elev
-        self.Demand = demand
-        self.Pattern = pattern
+    def __init__(self, id = "", elev = 0.0, demand = 0.0, pattern = "", description = ""):
+        self.ID = id if id  else ""
+        self.Elev = elev if elev else 0.0
+        self.Demand = demand if demand else 0.0
+        self.Pattern = pattern if pattern else ""
+        self.Description = description if description else ""
     
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Elev: <12}\t{self.Demand: <12}\t{self.Pattern: <15};"
+        return f" {self.ID: <15}\t{self.Elev: <12}\t{self.Demand: <12}\t{self.Pattern: <15};{self.Description}"
 
 class Reservoir():
     #ID              	Head        	Pattern
-    def __init__(self, id = 0, head = 0, pattern = ""):
-        self.ID = id
-        self.Head = head
-        self.Pattern = pattern
+    def __init__(self, id = "", head = 0.0, pattern = "", description = ""):
+        self.ID = id if id  else ""
+        self.Head = head if head else 0.0
+        self.Pattern = pattern if pattern else ""
+        self.Description = description if description else ""
     
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Head: <12}\t{self.Pattern: <15};"
+        return f" {self.ID: <15}\t{self.Head: <12}\t{self.Pattern: <15};{self.Description}"
 
 class Tank():
     #ID              	Elevation   	InitLevel   	MinLevel    	MaxLevel    	Diameter    	MinVol      	VolCurve
-    def __init__(self, id = 0, elevation = 0, initLevel = 0, minLevel = 0, maxLevel = 0, diameter = 0, minVol = 0, volCurve = 0):
-        self.ID = id
-        self.Elevation = elevation
-        self.InitLevel = initLevel
-        self.MinLevel = minLevel
-        self.MaxLevel = maxLevel
-        self.Diameter = diameter
-        self.MinVol = minVol
-        self.VolCurve = volCurve
+    def __init__(self, id = "", elevation = 0.0, initLevel = 0.0, minLevel = 0.0, maxLevel = 0.0, diameter = 0.0, minVol = 0.0, volCurve = 0.0, description = ""):
+        self.ID = id if id else ""
+        self.Elevation = elevation if elevation else 0.0
+        self.InitLevel = initLevel if initLevel else 0.0
+        self.MinLevel = minLevel if minLevel else 0.0
+        self.MaxLevel = maxLevel if maxLevel else 0.0
+        self.Diameter = diameter if diameter else 0.0
+        self.MinVol = minVol if minVol else 0.0
+        self.VolCurve = volCurve if volCurve else 0.0
+        self.Description = description if description else ""
         
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Elevation: <12}\t{self.InitLevel: <12}\t{self.MinLevel: <12}\t{self.MaxLevel: <12}\t{self.Diameter: <12}\t{self.MinVol: <12}\t{self.VolCurve: <15};"
+        return f" {self.ID: <15}\t{self.Elevation: <12}\t{self.InitLevel: <12}\t{self.MinLevel: <12}\t{self.MaxLevel: <12}\t{self.Diameter: <12}\t{self.MinVol: <12}\t{self.VolCurve: <15};{self.Description}"
 
 class Pipe():
     #ID              	Node1           	Node2           	Length      	Diameter    	Roughness   	MinorLoss   	Status
-    def __init__(self, id = 0, node1 = 0, node2 = 0, length = 0, diameter = 0, roughness = 0, minorLoss = 0, status = 0):
-        self.ID = id
-        self.Node1 = node1
-        self.Node2 = node2
-        self.Length = length
-        self.Diameter = diameter
-        self.Roughness = roughness
-        self.MinorLoss = minorLoss
-        self.Status = status
+    def __init__(self, id = "", node1 = "", node2 = "", length = 0.0, diameter = 0.0, roughness = 0.0, minorLoss = 0.0, status = "", description = ""):
+        self.ID = id if id else ""
+        self.Node1 = node1 if node1 else ""
+        self.Node2 = node2 if node2 else ""
+        self.Length = length if length else 0.0
+        self.Diameter = diameter if diameter else 0.0
+        self.Roughness = roughness if roughness else 0.0
+        self.MinorLoss = minorLoss if minorLoss else 0.0
+        self.Status = status if status else ""
+        self.Description = description if description else ""
         
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Length: <12}\t{self.Diameter: <12}\t{self.Roughness: <12}\t{self.MinorLoss: <12}\t{self.Status: <15};"
+        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Length: <12.4f}\t{self.Diameter: <12}\t{self.Roughness: <12}\t{self.MinorLoss: <12}\t{self.Status: <15};{self.Description}"
     
 class Pump():
     #ID              	Node1           	Node2           	Parameters
-    def __init__(self, id = 0, node1 = 0, node2 = 0, pattern = ""):
-        self.ID = id
-        self.Node1 = node1
-        self.Node2 = node2
-        self.Pattern = pattern
+    def __init__(self, id = "", node1 = 0, node2 = 0, pattern = "", description = ""):
+        self.ID = id if id else ""
+        self.Node1 = node1 if node1 else ""
+        self.Node2 = node2 if node2 else ""
+        self.Pattern = pattern if pattern else ""
+        self.Description = description if description else ""
         
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Pattern: <15};"
+        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Pattern: <15};{self.Description}"
 
 class Valve():
     #ID              	Node1           	Node2           	Diameter    	Type	Setting     	MinorLoss
-    def __init__(self, id = 0, node1 = 0, node2 = 0, diameter = 0, type = 0, setting = 0, minorLoss = 0):
-        self.ID = id
-        self.Node1 = node1
-        self.Node2 = node2
-        self.Diameter = diameter
-        self.Type = type
-        self.Setting = setting
-        self.MinorLoss = minorLoss
+    def __init__(self, id = "", node1 = 0.0, node2 = 0.0, diameter = 0.0, type = "", setting = "", minorLoss = 0.0, description = ""):
+        self.ID = id if id else ""
+        self.Node1 = node1 if node1 else ""
+        self.Node2 = node2 if node2 else ""
+        self.Diameter = diameter if diameter else 0.0
+        self.Type = type if type else ""
+        self.Setting = setting if setting else ""
+        self.MinorLoss = minorLoss if minorLoss else 0.0
+        self.Description = description if description else ""
         
     def __str__(self):
-        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Diameter: <12}\t{self.Type: <4}\t{self.Setting: <12}\t{self.MinorLoss: <15};"
+        return f" {self.ID: <15}\t{self.Node1: <17}\t{self.Node2: <17}\t{self.Diameter: <12}\t{self.Type: <4}\t{self.Setting: <12}\t{self.MinorLoss: <15};{self.Description}"
+    
+class Tag():
+    #Ejemplo de Tag NODE 	4               	Nodo_Terminal
+    #Para nodo, tanques embales el type es NODE y para tuberias, bombas y valvulas el type es LINK
+    def __init__(self, typeNode = "NODE", id = "", label = ""):
+        self.Type = typeNode if typeNode else "NODE"
+        self.ID = id if id else ""
+        self.Label = label if label else ""
+    
+    def __str__(self):
+        return f" {self.Type: <6}\t{self.ID: <16}\t{self.Label}"
     
 class Demand():
     #Junction        	Demand      	Pattern         	Category
-    def __init__(self, junction = 0, demand = 0, pattern = "", category = ""):
-        self.Junction = junction
-        self.Demand = demand
-        self.Pattern = pattern
-        self.Category = category
+    def __init__(self, junction = "", demand = 0.0, pattern = "", category = ""):
+        self.Junction = junction if junction else ""
+        self.Demand = demand if demand else 0.0
+        self.Pattern = pattern if pattern else ""
+        self.Category = category if category else ""
     
     def __str__(self):
         return f" {self.Junction: <15}\t{self.Demand: <12}\t{self.Pattern: <17}\t{self.Category: <15};"
     
+class Pattern():
+    #ID              	Multipliers
+    def __init__(self, description = "", id = "", multiplers = []):
+        self.Description = description if description else ""
+        self.ID = id if id else ""
+        self.Multipliers = multiplers
+    def __str__(self):
+        text = f";{self.Description}\n {self.ID: <16}\t"
+        i = 0
+        for item in self.Multipliers:
+            if (i < 5):
+                text += f"{item: <12}\t"
+            elif (i == 5):
+                text += f"{item: <12}\n"
+            else:
+                i = -1
+                text += f" {self.ID: <16}\t"
+            i += 1
+        return text
+    
 class Curve():
     #ID              	X-Value     	Y-Value
-    def __init__(self, id = 0, x_value = 0, y_value = 0):
-        self.ID = id
-        self.X_value = x_value
-        self.Y_value = y_value
+    def __init__(self, id = "", x_value = 0.0, y_value = 0.0):
+        self.ID = id if id else ""
+        self.X_value = x_value if x_value else 0.0
+        self.Y_value = y_value if y_value else 0.0
         
     def __str__(self):
         return f" {self.ID: <15}\t{self.X_value: <12}\t{self.Y_value: <15};"
     
-########################################################################Proximas clas #########################################################################
+######################### Proximas clas #####################################################
     
 class Coordinate():
     #Node            	X-Coord         	Y-Coord
-    def __init__(self, node = 0, x_coord = 0, y_coord = 0):
-        self.Node = node
-        self.X_Coord = x_coord
-        self.Y_Coord = y_coord
+    def __init__(self, node = "", x_coord = 0.0, y_coord = 0.0):
+        self.Node = node if node else ""
+        self.X_Coord = x_coord if x_coord else 0.0
+        self.Y_Coord = y_coord if y_coord else 0.0
     
     def __str__(self):
         return f" {self.Node: <15}\t{self.X_Coord: <16}\t{self.Y_Coord: <15};"
     
 class Vertice():
     #Link            	X-Coord         	Y-Coord
-    def __init__(self, link = 0, x_coord = 0, y_coord = 0):
-        self.Link = link
-        self.X_Coord = x_coord
-        self.Y_Coord = y_coord
+    def __init__(self, link = "", x_coord = 0.0, y_coord = 0.0):
+        self.Link = link if link else ""
+        self.X_Coord = x_coord if x_coord else 0.0
+        self.Y_Coord = y_coord if y_coord else 0.0
         
     def __str__(self):
         return f" {self.Link: <15}\t{self.X_Coord: <16}\t{self.Y_Coord: <15};"
     
 class Label():
     #X-Coord           Y-Coord          Label & Anchor Node
-    def __init__(self, x_coord = 0, y_coord = 0, anchor_node = ""):
-        self.X_Coord = x_coord
-        self.Y_Coord = y_coord
-        self.Anchor_node = anchor_node
+    def __init__(self, x_coord = 0.0, y_coord = 0.0, anchor_node = ""):
+        self.X_Coord = x_coord if x_coord else 0.0
+        self.Y_Coord = y_coord if y_coord else 0.0
+        self.Anchor_node = anchor_node if anchor_node else ""
         
     def __str__(self):
         return f" {self.X_Coord: <15}\t{self.Y_Coord: <12}\t{self.Anchor_node: <15};"
     
+class Backdrop():
+    def __init__(self, xmin = 0.0, ymin = 0.0, xmax = 10000.0, ymax = 10000.0, units = "Ninguno", file = "", offsetx = 0.0, offsety = 0.0):
+        self.x_min = xmin if xmin else 0.0
+        self.y_min = ymin if ymin else 0
+        self.x_max = xmax if xmax else 0.0
+        self.y_max = ymax if ymax else 0.0
+        self.Units = units if units else "Ninguno"
+        self.File = file if file else ""
+        self.offset_x = offsetx if offsetx else 0.0
+        self.offset_y = offsety if offsety else 0.0
+        
+    def __str__(self):
+        return f" DIMENSIONS     	{self.x_min: <17}\t{self.y_min: <17}\t{self.x_max: <17}\t{self.y_max: <17}\n UNITS          	{self.Units}\n FILE           	{self.File}\n OFFSET         	{self.offset_x: <17}\t{self.offset_y: <17}"
 ################################################################ Proximas class
