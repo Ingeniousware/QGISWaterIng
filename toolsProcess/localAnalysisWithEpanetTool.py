5d8a5e2a73edb2a07c10d3d7452a47441bbafe0d
carlosperezlantigua <cperezlantigua@gmail.com>
2025-04-13 12:24:30 -0400

    update 13042025 1224

--- toolsProcess/localAnalysisWithEpanetTool.py
+++ toolsProcess/localAnalysisWithEpanetTool.py
@@ -17,9 +17,17 @@
 """
 
 import datetime
+import os
+import shutil
+import uuid
 
-from watering_utils import WateringUtils
-
+from ..INP_Manager.dataType import Time_Options
+from ..INP_Manager.inp_options_enum import INP_Options
+from ..NetworkAnalysis.nodeNetworkAnalysisLocal import NodeNetworkAnalysisLocal
+from ..NetworkAnalysis.pipeNetworkAnalysisLocal import PipeNetworkAnalysisLocal
+from ..watering_utils import WateringUtils
+from ..INP_Manager.node_link_ResultType import LinkResultType, NodeResultType
+from ..INP_Manager.inp_utils import INP_Utils
 from ..QGISEpanet.epanet import EpanetSimulator
 from ..INP_Manager.INPManager import INPManager
 from .abstractAnalysisTool import AbstractAnalysisTool
@@ -52,10 +60,46 @@ class LocalAnalysisWithEpanetTool(AbstractAnalysisTool):
             dataTimeStr = date_time.translate(str.maketrans({":": "-", ".": "-"}))
             # tempFile = f"{fecha_hora}"
 
-
             inpManager = INPManager()
             inpManager.writeSections()
-            
+
+            inpFileTemp = INP_Utils.generate_directory(os.path.join(os.path.dirname(inpManager.OutFile), "Analysis", dataTimeStr))
+            inp_for_sim = inpFileTemp + f"\\{dataTimeStr}"
+
+            try:
+                shutil.copy(inpManager.OutFile, inp_for_sim)
+            except FileNotFoundError:
+                print("El archivo de origen no existe.")
+            except PermissionError:
+                print("No tienes permisos suficientes para copiar el archivo.")
+
             ep_sim = EpanetSimulator()
             results = ep_sim.run_sim(inpManager.OutFile)
-            inpFileTemp = INP_Utils.generate_directory(os.path.join(os.path.dirname(inpManager.OutFile), "Analysis", dataTimeStr))
+
+            node_result = NodeResultType.pressure
+            link_result = LinkResultType.flowrate
+
+            nodeResult_at_0hr = results.node[node_result.name].loc[0 * 3600, :]
+            linkResult_at_0hr = results.link[link_result.name].loc[0 * 3600, :]
+
+            analysis_id = str(uuid.uuid4())
+            time = datetime.now()
+            units = inpManager.options[INP_Options.Hydraulics].inpfile_units
+            nodeAnalysis = NodeNetworkAnalysisLocal(nodeResult_at_0hr, inpFileTemp, analysis_id, date_time, node_result)
+            nodeAnalysis.Execute()
+
+            pipeAnalysis = PipeNetworkAnalysisLocal(linkResult_at_0hr, inpFileTemp, analysis_id, date_time, units, link_result)
+            pipeAnalysis.Execute()
+
+            time: Time_Options = inpManager.options[INP_Options.Times]
+            _, rtime, _ = INP_Utils.hora_to_int(time.duration)
+            # print("---------------- Fin de los cáculos ------------------------")
+            if (rtime > 0):
+                for i in range(1, int(rtime)):
+                    analysis_id = str(uuid.uuid4())
+                    nodeResult_at_hr = self._results.node[node_result.name].loc[i * 3600, :]
+                    linkResult_at_hr = self._results.link[link_result.name].loc[0 * 3600, :]
+                    nodeAnalysis.elementAnalysisResults(nodeResult_at_hr, analysis_id)
+                    pipeAnalysis.elementAnalysisResults(linkResult_at_hr, analysis_id, units)
+
+            self.updateData(inpFileTemp, date_time)
